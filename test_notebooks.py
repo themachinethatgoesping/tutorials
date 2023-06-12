fd8490daf22e94187729b7dcfbb01b642ff7f00d
Peter Urban <peter.urban@ugent.be>
2023-06-12 18:52:32 +0200

    fix test errors

--- test_notebooks.py
+++ test_notebooks.py
@@ -3,15 +3,25 @@
 # Tutorial notebooks are pushed to git lfs!
 
 import os
+import shutil as sh
 import subprocess as sub
 from tqdm.auto import tqdm
 
 notebooks = []
 
+# delete all '.ipynb_checkpoints' folders
+del_folders = []
 for r,d,f in os.walk('.'):
-  if '.ipynb_checkpoints' in r:
-    continue
+	for folder in d:
+		if folder.endswith('.ipynb_checkpoints'):
+			del_folders.append(r + '/' + folder)
 
+for folder in del_folders:
+	if os.path.exists(folder):
+		# delete folder even if it is not empty
+		sh.rmtree(folder,ignore_errors=True)
+
+for r,d,f in os.walk('.'):
   for file in f:
     if file.endswith('.ipynb'):
       notebooks.append(r + '/' + file)
@@ -27,16 +37,9 @@ if os.path.exists('pytest.log'):
 		# call pytest --nbmake on each notebook
 # if pytest fails, exit and print error
 # if pytest succeeds, continue
-it = tqdm(notebooks)
-for notebook in it:
-	it.set_description(notebook)
-	try:
-		if sub.run(['pytest', '--nbmake', '--verbose', notebook], stdout=open('pytest.log', 'a')).returncode != 0:
-			exit()
-	except:
-		print('pytest failed on ' + notebook)
-		sub.run(['pytest', '--nbmake', notebook])
-		exit()
+print('Running pytest on each notebook...')
+if sub.run(['pytest', '--nbmake', *notebooks]).returncode != 0:
+	exit()
 
 
 # call jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace on each notebook
